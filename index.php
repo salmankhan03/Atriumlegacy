d7c6ec469d2460c61dd65c1038e0eb0365a5a396
mitesh000790 <mitesh000790@gmail.com>
2024-09-02 10:43:39 +0530

    Add structure of project

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="utf-8">
+        <title>Atriumlegacy</title>
+        <meta content="width=device-width, initial-scale=1.0" name="viewport">
+        <meta content="" name="keywords">
+        <meta content="" name="description">
+
+    </head>
+
+    <body>
+        <h1>Atriumlegacy</h1>
+    </body>
+
+</html>
