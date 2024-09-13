ad9c09d4ee9ee490738af8c1f74e6f3a40447c1c
Usman Kapadiya <hirenk@intellidt.com>
2024-09-13 16:07:58 +0530

    meta information add

--- initiative.php
+++ initiative.php
@@ -2,7 +2,7 @@
 <html lang="en">
    <head>
       <meta charset="utf-8">
-      <title>Atriumlegacy</title>
+      <title>Initiatives</title>
       <meta name="viewport" content="width=device-width, initial-scale=1">
       <meta content="" name="keywords">
       <meta content="" name="description">
