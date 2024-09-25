e80ecc4b276ea1c25aa1ffbacb528e81f38f5c3a
TonyHuang <hzx0109@gmail.com>
2024-09-25 12:40:58 -0700

    update page titles and adjust home page cards' padding

--- about_us.php
+++ about_us.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="utf-8">
-    <title>Atriumlegacy</title>
+    <title>Atrium Legacy Foundation</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta content="" name="keywords">
     <meta content="" name="description">
--- contact_us.php
+++ contact_us.php
@@ -2,7 +2,7 @@
     <html lang="en">
         <head>
             <meta charset="utf-8">
-            <title>Contact Us</title>
+            <title>Atrium Legacy Foundation</title>
             <meta name="viewport" content="width=device-width, initial-scale=1">
             <meta content="" name="keywords">
             <meta content="" name="description">
