2393ff02fe97f1ecaea87166f5abc65687937ad1
TonyHuang <hzx0109@gmail.com>
2024-11-01 12:57:02 -0700

    update force all pages to use HTTPS

--- send_donation_mail.php
+++ send_donation_mail.php
@@ -1,9 +1,5 @@
 <?php
 
-if (!isset($_SERVER['HTTPS']) || $_SERVER['HTTPS'] !== 'on') {
-    header("Location: https://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']);
-    exit();
-}
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 use PHPMailer\PHPMailer\SMTP;
