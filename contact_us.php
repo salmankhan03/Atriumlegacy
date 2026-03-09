fa5051ba413ca55386f2d86edaf97c616aa98b07
TonyHuang <hzx0109@gmail.com>
2026-03-09 11:10:10 -0700

    refactor contact_us page styles and update images

--- contact_us.php
+++ contact_us.php
@@ -17,18 +17,16 @@
     <link rel="icon" type="image/png" sizes="32x32" href="./img/icons/favicon/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="./img/icons/favicon/favicon-16x16.png">
     <link rel="manifest" href="./img/icons/favicon/site.webmanifest">
-    <style id='green-planet-retina-inline-css' type='text/css'>
+    <style>
         .contactUsBanner {
-            background-image: url('./img/contact_us/banner_contact.jpg');
+            background-image: url('./img/contact/banner_contact.jpg');
             background-position: top center;
             background-repeat: no-repeat;
             background-attachment: scroll;
             background-size: cover;
         }
 
-        /* Form Css */
-        /* Flexbox for columns layout */
-        .columns_wrap {
+        /* .columns_wrap {
             display: flex;
             flex-wrap: wrap;
             margin-bottom: 20px;
@@ -58,13 +56,11 @@
             width: 100%;
             padding: 10px;
             padding-left: 100px;
-            /* Adds space for the icon */
             border: none;
             border-bottom: 2px solid #ddd !important;
             font-size: 16px;
             transition: border-color 0.4s;
             background: none;
-            /* -webkit-border-radius:none !important */
         }
 
         .style-line input[type="text"]:focus,
@@ -110,7 +106,6 @@
 
         }
 
-        /* Bottom border hover effect with animation */
         input[type="text"]:hover,
         input[type="email"]:hover,
         input[type="tel"]:hover,
@@ -128,23 +123,12 @@
             }
         }
 
-        /* Placeholder styling */
         ::placeholder {
             color: #999;
             font-size: 14px;
         }
 
         input[type=submit] {
-            /* font-size: 16px;
-            line-height: 48px;
-            font-weight: 700;
-            padding-right: 35px;
-            padding-left: 35px;
-            border-width: 2px;
-            background-color: #4789cc !important;
-            color: #fff !important;
-            border-color: #4789cc !important;
-            font-family:'Montserrat' !important; */
             font-size: 16px !important;
             line-height: 50px !important;
             font-weight: 700 !important;
@@ -163,18 +147,11 @@
         }
 
         input[type=submit]:hover {
-            /* background-color: rgba(255, 255, 255, 0) !important;
-            color: #4789cc !important;
-            border-color: #4789cc !important; */
             background-color: #4789cc !important;
             border-color: #4789cc !important;
             color: #fff !important;
         }
 
-        /* Submit button */
-
-
-        /* Checkbox */
         .wpcf7-acceptance {
             display: flex;
             align-items: center;
@@ -185,7 +162,6 @@
             margin-right: 10px;
         }
 
-        /* Responsive design */
         @media (max-width: 768px) {
             .column-1_2 {
                 width: 100%;
@@ -193,7 +169,6 @@
             }
         }
 
-        /* Custom  */
         input:not([type=button]):not([type=checkbox]):not([type=file]):not([type=hidden]):not([type=image]):not([type=radio]):not([type=reset]):not([type=submit]):not([type=color]):not([type=range]) {
             height: 49px;
             -webkit-border-radius: none !important;
@@ -223,9 +198,6 @@
             font-family: 'Montserrat' !important;
         }
 
-        /* Form css  */
-
-        /* Toast Notification Styles */
         .toast-notification {
             position: fixed;
             top: 80px;
@@ -277,20 +249,13 @@
 
         .toast-notification.hide {
             animation: slideOut 0.3s ease-in;
-        }
+        } */
     </style>
 </head>
 
 <body>
-    <!-- Toast Notification -->
-    <div id="toast" class="toast-notification"></div>
-
     <?php include 'header.php'; ?>
     <div class="middle_inner">
-        <!-- <div class="content_wrap fullwidth">
-            <div class="middle_content entry"></div>
-        </div> -->
-        <!-- Banner -->
         <div class="contactUsBanner">
             <div class="container text-center banner-paddings">
                 <h2 class="banner-header text-center">Contact Us</h2>
@@ -304,193 +269,21 @@
                 <h2 class="px-4 italic-heading text-center">We are here to connect, listen, and collaborate. Whether you are a senior, family member, volunteer, partner, or community organization, we invite you to reach out and learn more about our work. Atrium Legacy Foundation is committed to building meaningful relationships and working together to strengthen support for seniors and caregivers.</h2>
             </div>
 
-            <div class="row">
-                <div class="col-md-4">
-                    <div class="py-5 p-xs-0">
-                        <h2 class="h2-title" style="font-size:2rem">Contact Details</h2>
-                        <div class="text-content-space">
-                            <h4 class="text-content mb-3">
-                                200-3071 No.5 Rd <br />
-                                Richmond BC <br />
-                                V6X 2T4
-                            </h4>
-                            <h3 class="mb-3">
-                                <a class="text-content" href="mailto:inquiry@atriumlegacy.com" id="email">
-                                    inquiry@atriumlegacy.com
-                                </a>
-                            </h3>
-                            <!-- <h2 class="h2-title" style="font-size:1.5rem">+1 (840) 841-2569</h2> -->
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-8 text-content">
-                    <div class="py-5 p-xs-0">
-                        <form action="send_mail.php" method="post" class="wpcf7-form init" aria-label="Contact form" novalidate="novalidate" data-status="init" data-inited-validation="1">
-                            <div style="display: none;">
-                                <input type="hidden" name="_wpcf7" value="1324">
-                                <input type="hidden" name="_wpcf7_version" value="5.9.8">
-                                <input type="hidden" name="_wpcf7_locale" value="en_US">
-                                <input type="hidden" name="_wpcf7_unit_tag" value="wpcf7-f1324-p18859-o1">
-                                <input type="hidden" name="_wpcf7_container_post" value="18859">
-                                <input type="hidden" name="_wpcf7_posted_data_hash" value="">
-                            </div>
-                            <div class="">
-                                <div class="columns_wrap">
-                                    <div class="column-1_2">
-                                        <p>
-                                            <span class="style-line icon-name">
-                                                <span class="wpcf7-form-control-wrap" data-name="your-name">
-                                                    <input size="40" maxlength="400" class="wpcf7-form-control wpcf7-text wpcf7-validates-as-required fill_inited" aria-required="true" aria-invalid="false" placeholder="Name" value="" type="text" name="your-name" style="border: none !important;border-radius: 0px !important;border-bottom: 1px solid #ddd !important;background-color: transparent">
-                                                    <span class="line"></span>
-                                                </span>
-                                            </span>
-                                        </p>
-                                    </div>
-                                    <div class="column-1_2">
-                                        <p>
-                                            <span class="style-line icon-email">
-                                                <span class="wpcf7-form-control-wrap" data-name="your-email">
-                                                    <input size="40" maxlength="400" class="wpcf7-form-control wpcf7-email wpcf7-validates-as-required wpcf7-text wpcf7-validates-as-email fill_inited" aria-required="true" aria-invalid="false" placeholder="Email Address" value="" type="email" name="your-email" style="border: none !important;border-radius: 0px !important;border-bottom: 1px solid #ddd !important;background-color: transparent">
-                                                    <span class="line"></span>
-                                                </span>
-                                            </span>
-                                        </p>
-                                    </div>
-                                </div>
-                                <div class="columns_wrap">
-                                    <div class="column-1_2">
-                                        <p>
-                                            <span class="style-line icon-phone">
-                                                <span class="wpcf7-form-control-wrap" data-name="phone">
-                                                    <input size="40" maxlength="400" class="wpcf7-form-control wpcf7-tel wpcf7-validates-as-required wpcf7-text wpcf7-validates-as-tel fill_inited" aria-required="true" aria-invalid="false" placeholder="Phone" value="" type="tel" name="phone" style="border: none !important;border-radius: 0px !important;border-bottom: 1px solid #ddd !important;background-color: transparent">
-                                                    <span class="line"></span>
-                                                </span>
-                                            </span>
-                                        </p>
-                                    </div>
-                                    <div class="column-1_2">
-                                        <p>
-                                            <span class="style-line icon-subject">
-                                                <span class="wpcf7-form-control-wrap" data-name="subject">
-                                                    <input size="40" maxlength="400" class="wpcf7-form-control wpcf7-text wpcf7-validates-as-required fill_inited" aria-required="true" aria-invalid="false" placeholder="Subject" value="" type="text" name="subject" style="border: none !important;border-radius: 0px !important;border-bottom: 1px solid #ddd !important;background-color: transparent">
-                                                    <span class="line"></span>
-                                                </span>
-                                            </span>
-                                        </p>
-                                    </div>
-                                </div>
-                                <div class="columns_wrap margin-bottom">
-                                    <div class="column-1_1">
-                                        <p>
-                                            <span class="style-line icon-textarea">
-                                                <span class="wpcf7-form-control-wrap" data-name="your-message" style="width:100%">
-                                                    <textarea cols="40" rows="3" maxlength="2000" class="wpcf7-form-control wpcf7-textarea wpcf7-validates-as-required fill_inited" aria-required="true" aria-invalid="false" placeholder="How can we help you? Feel free to get in touch!" name="your-message" style="border: none !important;border-radius: 0px !important;border-bottom: 1px solid #ddd !important;background-color: transparent"></textarea>
-                                                    <span class="line"></span>
-                                                </span>
-                                            </span>
-                                        </p>
-                                    </div>
-                                </div>
-                                <div class="columns_wrap margin-bottom">
-                                    <div class="column-1_1">
-                                        <p>
-                                            <span class="wpcf7-submit-style">
-                                                <input type="hidden" name="recaptcha_token" id="recaptchaToken">
-                                                <input class="wpcf7-form-control wpcf7-submit has-spinner custom-btn3" type="submit" value="Get In Touch" disabled>
-                                                <span class="submit-style-in"></span>
-                                                <label class="ml-3 pt-3">
-                                                    <input type="checkbox" name="acceptance" value="1" aria-invalid="false" class="inited" id="acceptanceCheckbox">
-                                                    <span class="wpcf7-list-item-label checkbox_position wpcf7-form-control">I agree that my data is
-                                                        <a class="wpcf7-form-control" href="privacy.php" style="color:#4789cc; text-decoration:underline;">collected and stored</a>.
-                                                    </span>
-                                                </label>
-                                            </span>
-                                        </p>
-                                        <p>We respect your privacy. All information shared with us will be handled confidentially and used only to respond to your inquiry or support our community initiatives.</p>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="wpcf7-response-output" aria-hidden="true"></div>
-                        </form>
-                    </div>
-                </div>
+            <div class="bottom-padding text-center">
+                <!-- <h2 class="h2-title" style="font-size:2rem">Contact Details</h2> -->
+                <h4 class="text-content mb-3">
+                    200-3071 No.5 Rd Richmond BC V6X 2T4
+                </h4>
+                <h3>
+                    <a class="text-content" href="mailto:inquiry@atriumlegacy.com" id="email">
+                        inquiry@atriumlegacy.com
+                    </a>
+                </h3>
             </div>
         </div>
     </div>
 
     <?php include 'footer.php'; ?>
-    <script src="https://www.google.com/recaptcha/api.js?render=6LcdmPkrAAAAAIwGqAkl04My2kr5fYy-UlDNaMyZ"></script>
-    <script>
-        // Toast notification function
-        function showToast(message, isError = false) {
-            const toast = document.getElementById('toast');
-            toast.textContent = message;
-            toast.className = 'toast-notification show' + (isError ? ' error' : '');
-
-            setTimeout(function() {
-                toast.classList.add('hide');
-                setTimeout(function() {
-                    toast.classList.remove('show', 'hide');
-                }, 300);
-            }, 4000);
-        }
-
-        // Get the checkbox and the submit button elements
-        const checkbox = document.getElementById('acceptanceCheckbox');
-        const submitButton = document.querySelector('.wpcf7-submit');
-        const form = document.querySelector('.wpcf7-form');
-
-        // Add an event listener to the checkbox to monitor changes
-        checkbox.addEventListener('change', function() {
-            submitButton.disabled = !checkbox.checked;
-        });
-
-        // Handle form submission with reCAPTCHA v3 and AJAX
-        form.addEventListener('submit', function(e) {
-            e.preventDefault();
-
-            // Disable submit button to prevent multiple submissions
-            submitButton.disabled = true;
-            submitButton.value = 'Sending...';
-
-            grecaptcha.ready(function() {
-                grecaptcha.execute('6LcdmPkrAAAAAIwGqAkl04My2kr5fYy-UlDNaMyZ', {
-                    action: 'submit'
-                }).then(function(token) {
-                    document.getElementById('recaptchaToken').value = token;
-
-                    // Prepare form data
-                    const formData = new FormData(form);
-
-                    // Send AJAX request
-                    fetch('send_mail.php', {
-                            method: 'POST',
-                            body: formData
-                        })
-                        .then(response => response.text())
-                        .then(data => {
-                            // Check if the response contains success message
-                            if (data.includes('Thank you for contacting us')) {
-                                showToast('Thank you for contacting us. We will get back to you shortly.');
-                                form.reset(); // Clear the form
-                                checkbox.checked = false;
-                                submitButton.disabled = true;
-                            } else {
-                                showToast('Message could not be sent. Please try again.', true);
-                            }
-                        })
-                        .catch(error => {
-                            showToast('An error occurred. Please try again.', true);
-                            console.error('Error:', error);
-                        })
-                        .finally(() => {
-                            submitButton.disabled = false;
-                            submitButton.value = 'Get In Touch';
-                        });
-                });
-            });
-        });
-    </script>
 </body>
 
 </html>
